eac8db83f04bfd37239995d843c02de931d1eb3c
KAALVIVEK <kaalvivek9@gmail.com>
2025-01-19 17:49:50 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -16,14 +16,14 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '7942937704:AAFM6qI8dd74bEuSu-E0UUqN0N9FioD4qa8'
+TOKEN = '7254039866:AAFv-OEkakQTkfK1e6RG5uQW-4HzmQbqCJA'
 MONGO_URI = 'mongodb+srv://sharp:sharp@sharpx.x82gx.mongodb.net/?retryWrites=true&w=majority&appName=SharpX'
 FORWARD_CHANNEL_ID = -1002383157063
 CHANNEL_ID = -1002383157063
 error_channel_id = -1002383157063
 ADMIN_ID = 7083378335
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-USERNAME = "@TREXVIVEK"
+USERNAME = "@STARxGHOST"
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client['sharp']
 users_collection = db.users
